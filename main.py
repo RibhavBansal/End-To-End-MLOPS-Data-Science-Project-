54485594646ca5e880d53a08de0970ad363bc3d6
Ribhav Bansal <96466752+RibhavBansal@users.noreply.github.com>
2024-04-11 16:10:19 +0530

    commit-5

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME1 = "Data Ingestion stage"
 
 try:
-    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<")
+    logger.info(f">>>>>>>> stage {STAGE_NAME1} started <<<<<<<<")
     obj = DataIngestionTrainingPipeline()
     obj.main()
-    logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<< \n\n x=============x")
+    logger.info(f">>>>>>>> stage {STAGE_NAME1} completed <<<<<<<< \n\n x=============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME2 = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME2} started <<<<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>>>> stage {STAGE_NAME2} completed <<<<<<<< \n\n x=============x")
 except Exception as e:
     logger.exception(e)
     raise e
